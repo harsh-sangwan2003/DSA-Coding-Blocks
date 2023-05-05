b71903e548ac8d49f317de6ae4f31b5206f23b1b
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-05-05 19:55:49 +0530

    added

--- March/Basic_Of_Programming/Getting_Started/Lec_01/demo_01.java
+++
@@ -1,9 +0,0 @@
-package March.Basic_Of_Programming.Getting_Started.Lec_01;
-
-public class demo_01 {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello Java");
-    }
-}
